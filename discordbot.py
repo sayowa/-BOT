cb457a1fa66d19fec5d13683f92c6505d40c5255
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 16:20:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,36 +1,24 @@
-import discord
-from discord.ext import commands
-import os
-import asyncio
-
-# 通知用のチャンネルID
-inform_channel = '#聞き専tc'
+import discord 
+import datetime 
+ 
+client = discord.Client() 
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-# bot
-bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix:'/'
-
-# 誰かがvoice channelに出入りしたら発火
-bot.voice_state_update do |event|
-    # 発火させたユーザー名を取得
-    user = event.user.name
-
-    # もしデータが空だと抜けていったチャンネルを取得
-    if event.channel == nil then
-        # チャンネル名を取得
-        channel_name = event.old_channel.name
-
-        # 退出したことをinform_channelに通知
-        bot.send_message(inform_channel, "@everyone #{user} が #{channel_name}を出たで～")
-    else
-        # チャンネル名を取得
-        channel_name = event.channel.name
-
-        # 入室したことをinform_channelに通知
-        bot.send_message(inform_channel, "@everyone #{user} が #{channel_name}に入ったで～")
-    end
-end
+@client.event 
+async def on_voice_state_update(before, after): 
+    if after.server.id == '監視するサーバーid': 
+        nowtime = datetime.datetime.utcnow() 
+        nowtime = nowtime + datetime.timedelta(hours=9) 
+        nowtime = nowtime.strftime("%m/%d-%H:%M") 
+        vcchannel = client.get_channel('通知させたいテキストチャンネルid') 
+ 
+        if(before.voice_channel is None): 
+            jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
+            await client.send_message(vcchannel, jointext) 
+        elif(after.voice_channel is None): 
+            outtext=nowtime + "に　"+ before.name + "　が　"+ before.voice_channel.name + " から退出しました。" 
+            await client.send_message(vcchannel, outtext) 
 
 client.run(token)
 
