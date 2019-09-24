e95dd513fc9b11f780bdd0b1f0e8a7b2816242c3
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 16:13:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,73 +3,34 @@ from discord.ext import commands
 import os
 import asyncio
 
-client = commands.Bot(command_prefix='/')
+# 通知用のチャンネルID
+inform_channel = 625682764431097861
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+# bot
+bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix:'/'
 
-@client.command()
-async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
-    cnt, settime = int(cnt), float(settime*60)
-    reaction_member = ["参加者一覧"]
-    test = discord.Embed(title=about,colour=0x1e90ff)
-    test.add_field(name=f"あと{cnt}人 募集中\n", value='\n'.join(reaction_member), inline=True)
-    msg = await ctx.send(embed=test)
-    #投票の欄
-    await msg.add_reaction('⏫')
-    await msg.add_reaction('⏬')
+# 誰かがvoice channelに出入りしたら発火
+bot.voice_state_update do |event|
+    # 発火させたユーザー名を取得
+    user = event.user.name
 
-    def check(reaction, user):
-        emoji = str(reaction.emoji)
-        if user.bot == True:    # botは無視
-            pass
-        else:
-            return emoji == '⏫' or emoji == '⏬'
+    # もしデータが空だと抜けていったチャンネルを取得
+    if event.channel == nil then
+        # チャンネル名を取得
+        channel_name = event.old_channel.name
 
-    while 0 <= cnt:
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
-        except asyncio.TimeoutError:
-            await ctx.send('募集時間が過ぎたｿﾞ、再度、募集ｵﾅｼｬｽ!')
-            break
-        else:
-            print(str(reaction.emoji))
-            if str(reaction.emoji) == '⏫':
-                #if user.name in reaction_member:
-                    #await msg.remove_reaction(str(reaction.emoji), user)
-                    #continue
-                reaction_member.append(user.name)
-                cnt -= 1
-                test = discord.Embed(title=about,colour=0x1e90ff)
-                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                await msg.edit(embed=test)
+        # 退出したことをinform_channelに通知
+        bot.send_message(inform_channel, "@everyone #{user} が #{channel_name}を出たで～")
+    else
+        # チャンネル名を取得
+        channel_name = event.channel.name
 
-                if cnt == 0:
-                    test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                    await msg.edit(embed=test)
-                    finish = discord.Embed(title=about,colour=0x1e90ff)
-                    finish.add_field(name="メンバーがきまったようｿﾞ\n復活の魂わすれるなよ！", value='\n'.join(reaction_member),inline=True)
-                    await ctx.send(embed=finish)
-
-            elif str(reaction.emoji) == '⏬':
-                if user.name in reaction_member:
-                    reaction_member.remove(user.name)
-                    cnt += 1
-                    test = discord.Embed(title=about,colour=0x1e90ff)
-                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                    await msg.edit(embed=test)
-                else:
-                    pass
-        # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
-        await msg.remove_reaction(str(reaction.emoji), user)
-
-
+        # 入室したことをinform_channelに通知
+        bot.send_message(inform_channel, "@everyone #{user} が #{channel_name}に入ったで～")
+    end
+end
 
 client.run(token)
 
