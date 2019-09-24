badcd434767029765fd691c6ae78ee52adc09f5c
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 16:28:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,21 @@
-import discord 
-import datetime 
- 
-client = discord.Client() 
+import discord
+from datetime import datetime, timedelta
+
+client = discord.Client()
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event 
-async def on_voice_state_update(before, after): 
-    if after.server.id == '625656760501272617': 
-        nowtime = datetime.datetime.utcnow() 
-        nowtime = nowtime + datetime.timedelta(hours=9) 
-        nowtime = nowtime.strftime("%m/%d-%H:%M") 
-        vcchannel = client.get_channel('625682764431097861') 
- 
-        if(before.voice_channel is None): 
-            jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
-            await client.send_message(vcchannel, jointext) 
-        elif(after.voice_channel is None): 
-            outtext=nowtime + "に　"+ before.name + "　が　"+ before.voice_channel.name + " から退出しました。" 
-            await client.send_message(vcchannel, outtext) 
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 625656760501272617 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(625682764431097861)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
 client.run(token)
 
