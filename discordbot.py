2ebae270a62f11ab8d84688c5c02c9668dd37c35
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 16:22:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,11 +7,11 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event 
 async def on_voice_state_update(before, after): 
-    if after.server.id == '監視するサーバーid': 
+    if after.server.id == '625656760501272617': 
         nowtime = datetime.datetime.utcnow() 
         nowtime = nowtime + datetime.timedelta(hours=9) 
         nowtime = nowtime.strftime("%m/%d-%H:%M") 
-        vcchannel = client.get_channel('通知させたいテキストチャンネルid') 
+        vcchannel = client.get_channel('625682764431097861') 
  
         if(before.voice_channel is None): 
             jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
