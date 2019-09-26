cb8a3f83a4fcbd06d33696a6fd125bf29bad80b7
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-26 18:37:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,7 +25,7 @@ async def on_member_join(member):
     guild = member.guild # サーバー
     sysch = guild.system_channel # 参加メッセージを表示するチャンネル
     if sysch: # チャンネルが設定されてなかったら何もしない
-        text = f'{member.mention} いらっしゃいませ。お約束のテキストチャンネル読んでくださいね。'
+        text = f'{member.mention} さん　いらっしゃいませ。お約束のテキストチャンネル読んでくださいね。'
         await sysch.send(text)
 
 @client.event
@@ -33,7 +33,7 @@ async def on_member_remove(member):
     guild = member.guild # サーバー
     sysch = guild.system_channel # 参加メッセージを表示するチャンネル
     if sysch: # チャンネルが設定されてなかったら何もしない
-        text = f'{member.name} 寂しいですが、またよかったら来てくださいね'
+        text = f'{member.name} さん　寂しいですが、またよかったら来てくださいね'
         await sysch.send(text)
 
 client.run(token)
